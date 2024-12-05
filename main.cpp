df981d496d3b7d286e7569900fa87213be6a21c9
Tikhon20043 <144627214+Tikhon20043@users.noreply.github.com>
2024-12-05 14:04:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include "max_finder.h"
 
+int findMax(int* arr, int size, int (*compare)(int, int)) {
+    if (size <= 0) return -1;
+
+    int maxElement = arr[0];
+    for (int i = 1; i < size; ++i) {
+        if (compare(arr[i], maxElement) > 0) {
+            maxElement = arr[i];
+        }
+    }
+    return maxElement;
+}
+
+int compare(int a, int b) {
+    return a - b;
+}
+
+int main() {
+    int arr[] = {3, 5, 2, 8, 6};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    int maxElement = findMax(arr, size, compare);
+    
+    std::cout << "Максимальный элемент в массиве: " << maxElement << std::endl;
+
+    return 0;
+}
